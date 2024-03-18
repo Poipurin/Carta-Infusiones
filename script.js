99d648f2efad8e988f3c8e4b9825fc3f23aef637
Poipurin <b.pinorojo@gmail.com>
2024-03-18 03:33:51 -0300

    js

---
+++ script.js
@@ -0,0 +1,19 @@
+window.onscroll = function() {miFuncionFija()};
+
+function miFuncionFija() {
+  var elementoFijo = document.querySelector('.Opciones');
+  var botonesExtra = document.querySelectorAll('.btnScroll');
+  
+  if (window.pageYOffset > 100) {
+    elementoFijo.classList.add("fixed");
+    
+    botonesExtra.forEach(function(btn) {
+      btn.style.display = "inline-block"; 
+    });
+  } else {
+    elementoFijo.classList.remove("fixed");
+    botonesExtra.forEach(function(btn) {
+      btn.style.display = "none";
+    });
+  }
+}
